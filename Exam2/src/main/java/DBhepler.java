dababd17b017325e4774724051f50fa04c8f9242
zhaotianxin <ztxemail@163.com>
2019-07-15 16:07:17 +0800

    aa

--- Exam2/src/main/java/DBhepler.java
+++ Exam2/src/main/java/DBhepler.java
@@ -1,21 +1,27 @@
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBhepler{
-    public static final String url = "jdbc:mysql://127.0.0.1/sqlDate";
-    public static final String name = "com.mysql.jdbc.Driver";
-    public static final String user = "root";
-    public static final String password = "12345";
+//    public static final String url = "jdbc:mysql://127.0.0.1/sqlDate";
+//    public static final String name = "com.mysql.jdbc.Driver";
+//    public static final String user = "root";
+//    public static final String password = "12345";
 
     public Connection conn = null;
     public PreparedStatement pst = null;
 
     public DBhepler(String sql) {
         try {
-            Class.forName(name);//指定连接类型
-            conn = DriverManager.getConnection(url,user,password);//获取连接
+            Properties properties=new Properties();
+            InputStream is=new FileInputStream("/db.properties");
+            properties.load(is);
+            Class.forName(properties.getProperty("driverClass"));//指定连接类型
+            conn = DriverManager.getConnection(properties.getProperty("url"),properties.getProperty("user"),properties.getProperty("password"));//获取连接
             pst = conn.prepareStatement(sql);//准备执行语句
         } catch (Exception e) {
             e.printStackTrace();
